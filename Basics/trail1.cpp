43c440eaa0166dedb084416704e0994111e1d6ac
Sriram <65888303+Sriram-2210@users.noreply.github.com>
2022-08-12 02:51:11 +0000

    Commit

--- Basics/trail1.cpp
+++ Basics/trail1.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
-int main(void){
-    
+int main(){
+    int n = 5;
+    for(int i = 0; i<n; i++) cout << "The number is " << i <<"\n";
+    return 0;
 }
+
+// here in c++, int main(void) and int main() won't make any difference as both are same.
+// In c, however, int main(void) is the recommended practice as it can take any no of arguments 
