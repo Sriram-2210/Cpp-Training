a27c0269953cc61c627fe9e954c9a75a4499c0d8
Sriram <65888303+Sriram-2210@users.noreply.github.com>
2022-08-20 02:51:47 +0000

    Commit

--- Basics/nestedif.cpp
+++ Basics/nestedif.cpp
@@ -5,10 +5,15 @@ int main(){
     cout << "Enter i value: ";
     cin >> i;
     if (i>10){
-        if (i%2!=0) cout << "next step" << endl;
+        if (i==50) cout << "equal" << endl;
         if(i<50) cout << i << " is lower" << endl;
         else cout << i << " is greater" << endl;
     }
     else cout << i << " is lowest" << endl;
     return 0;
 }
+
+/* 
+in nested if statements, irrespective of first if condition the nested block executes
+
+*/
